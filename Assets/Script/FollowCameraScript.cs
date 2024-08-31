1835c62aa5fdfe913708f253d290ffea1507f199
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2024-08-31 18:44:11 +0900

    プレイヤーを中心にとらえ続けるカメラを実装

--- Assets/Script/FollowCameraScript.cs
+++ Assets/Script/FollowCameraScript.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class FollowCameraScript : MonoBehaviour
 {
-    // プレイヤーを追従する
-    public Transform player;
-    // オブジェクトとのオフセット
-    public Vector3 offset;
+	// プレイヤーを追従する
+	public Transform player;
+	// オブジェクトとのオフセット
+	public Vector3 offset;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
 		// 初期オフセットの計算
 		offset = transform.position - player.position;
+		// カーソルを中央に固定して表示を消す
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		// プレイヤーを中心にカメラを回転
-		Quaternion rotation = Quaternion.Euler(0, 0, 0);
-		transform.position = player.position + rotation * offset;
-		transform.LookAt(player.position);
+	// Update is called once per frame
+	void Update()
+	{
+
+		float mx = Input.GetAxis("Mouse X");
+		float my = Input.GetAxis("Mouse Y");
+
+		// X方向に一定量移動していれば横回転
+		if (0.01f < Mathf.Abs(mx))
+		{
+			// 回転軸はワールド座標のY軸
+			transform.RotateAround(player.position, Vector3.up, mx);
+		}
+
+		// Y方向に一定量移動していれば縦回転
+		if (0.01f < Mathf.Abs(my))
+		{
+			// 回転軸はカメラ自身のX軸
+			transform.RotateAround(player.position, transform.right, -my);
+		}
+		// プレイヤーに追従させる
+		transform.position = player.position - transform.forward * offset.magnitude;
 	}
 }
